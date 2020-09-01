d14a093d39d514e431687ba36327f10649d8f8c2
MISHI RAY <42464035+mishiray@users.noreply.github.com>
2020-09-01 20:46:20 +0100

    initial commit

---
+++ includes/header.blade.php
@@ -0,0 +1,42 @@
+<header class="header">
+    <div class="header-middle sticky-header">
+        <div class="container">
+            <div class="header-left">
+                <button class="mobile-menu-toggler">
+                    <span class="sr-only">Toggle mobile menu</span>
+                    <i class="icon-bars"></i>
+                </button>
+                    <a href="{{ url('/') }}" class="navbar-brand"><img height="25" width="105" src="{{ $web_source }}/images/greener_logo.png" /></a>
+                <nav class="main-nav">
+                    <ul class="menu">
+                        <li class="megamenu-container">
+                            <a class="sf-with-ul" href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li>
+                            <a class="sf-with-ul" href="{{ route('shop') }}">Shop</a>
+                        </li>
+                    </ul><!-- End .menu -->
+                </nav><!-- End .main-nav -->
+            </div><!-- End .header-left -->
+
+            <div class="header-right">
+                <div class="header-search">
+                    <a href="#" class="search-toggle" role="button" title="Search"><i class="icon-search"></i></a>
+                    <form action="#" method="get">
+                        <div class="header-search-wrapper">
+                            <label for="q" class="sr-only">Search</label>
+                            <input type="search" class="form-control" name="q" id="q" placeholder="Search in..." required>
+                        </div><!-- End .header-search-wrapper -->
+                    </form>
+                </div><!-- End .header-search -->
+                
+                <div class="dropdown cart-dropdown">
+                    <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static">
+                        <i class="icon-shopping-cart"></i>
+                        <span class="cart-count">#</span>
+                    </a>
+                </div><!-- End .cart-dropdown -->
+            </div><!-- End .header-right -->
+        </div><!-- End .container -->
+    </div><!-- End .header-middle -->
+</header><!-- End .header -->
